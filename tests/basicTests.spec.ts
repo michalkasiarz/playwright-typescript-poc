ff2989da9531574a23a917fc64f7331924018f1e
Michal Kasiarz <michal.kasiarz@protonmail.com>
2022-07-02 14:26:10 +0200

    beforeEach hooks added

--- tests/basicTests.spec.ts
+++ tests/basicTests.spec.ts
@@ -1,8 +1,11 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('Example.com basic test suite @regressionExample', () => {
-  test('Page heading test', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto('https://www.example.com')
+  })
+
+  test('Page heading test', async ({ page }) => {
     const pageHeading = await page.locator('h1')
     const pageHeadingScreenshot = await page.$('h1')
     await expect(pageHeading).toHaveText('Example Domain')
@@ -10,31 +13,29 @@ test.describe('Example.com basic test suite @regressionExample', () => {
   })
 
   test('Page heading visibility test', async ({ page }) => {
-    await page.goto('https://www.example.com')
     const pageHeading = await page.locator('h1')
     await expect(pageHeading).toBeVisible()
   })
 
   test('Page title test', async ({ page }) => {
-    await page.goto('https://www.example.com')
     await expect(page).toHaveTitle('Example Domain')
   })
 
   test('URL no redirection test', async ({ page }) => {
-    await page.goto('https://www.example.com')
     await expect(page).toHaveURL('https://www.example.com')
   })
 
   test('Removed header non-visibility test', async ({ page }) => {
-    await page.goto('https://www.example.com')
     const removedHeader = page.locator('text=Removed Domain Name')
     await expect(removedHeader).not.toBeVisible()
   })
 })
 
 test.describe('Zero.webappsecurity basic test suite @regressionZeroWebApp', () => {
-  test('No credentials entered error message test', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto('http://zero.webappsecurity.com/')
+  })
+  test('No credentials entered error message test', async ({ page }) => {
     await page.click('#signin_button')
     await page.click('text=Sign in')
 
@@ -43,7 +44,6 @@ test.describe('Zero.webappsecurity basic test suite @regressionZeroWebApp', () =
   })
 
   test('Invalid credentials error message test', async ({ page }) => {
-    await page.goto('http://zero.webappsecurity.com/')
     await page.click('#signin_button')
 
     await page.type('#user_login', 'testuser')
