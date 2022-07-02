4d4fabf861d456f54134f057c203ccb4be15da42
michalkasiarz <42114632+michalkasiarz@users.noreply.github.com>
2022-07-02 16:15:00 +0200

    Tests/basic tests (#3)
    * prettier config modified
    
    * tests directory created along with one basic test
    
    * basic testfile renamed, test names refactored
    
    * new tests added, text assertion check fixed
    
    * organizing tests into suites
    
    * tests suites tagged as regression
    
    * playwright config file added
    
    * screenshot capture added
    
    * beforeEach hooks added
    
    * helpers functions added, code refactored
    
    * node scripts added for running tests
    
    * test suites separates into different
    
    * video and screenshot capture turned on on fail, concurrent test run setup

---
+++ helpers.ts
@@ -0,0 +1,9 @@
+import { expect } from '@playwright/test'
+
+export async function loadHomepage(page, url) {
+  await page.goto(url)
+}
+
+export async function assertTextForElement(page, selector, expectedText) {
+  await expect(page.locator(selector)).toHaveText(expectedText)
+}
