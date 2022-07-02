4c8022dde10ca16fb8be41710369a86b2b9eb4d4
michalkasiarz <42114632+michalkasiarz@users.noreply.github.com>
2022-07-02 18:12:48 +0200

    Tests/e2e config (#6)
    * basic e2e tests created, relevant node run script added to package.json
    
    * logout part e2e test steps added
    
    * parallel execution flag added

---
+++ tests/e2e/e2e-login.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test'
+
+test.describe.parallel('Login and logout flow', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://zero.webappsecurity.com/')
+  })
+
+  test('Login - negative scenario', async ({ page }) => {
+    await page.click('#signin_button')
+    await page.type('#user_login', 'dummy_user')
+    await page.type('#user_password', 'dummypassword')
+    await page.click('text=Sign in')
+
+    const errorMessage = page.locator('.alert-error')
+    await expect(errorMessage).toHaveText('Login and/or password are wrong.')
+  })
+
+  test('Login and logout - positive scenario', async ({ page }) => {
+    await page.click('#signin_button')
+    await page.type('#user_login', 'username')
+    await page.type('#user_password', 'password')
+    await page.click('text=Sign in')
+
+    await page.goto('http://zero.webappsecurity.com/bank/account-summary.html')
+
+    const accountSummaryTab = await page.locator('#account_summary_tab')
+    await expect(accountSummaryTab).toBeVisible()
+
+    await page.click('text=username')
+    await page.click('text=Logout')
+
+    await expect(page).toHaveURL('http://zero.webappsecurity.com/index.html')
+  })
+})
