9149113a9e1aefebab7823c917d6ab643d95753f
Michal Kasiarz <michal.kasiarz@protonmail.com>
2022-07-02 12:40:44 +0200

    new tests added, text assertion check fixed

--- tests/basicTests.spec.ts
+++ tests/basicTests.spec.ts
@@ -1,9 +1,20 @@
 import { test, expect } from '@playwright/test'
 
+test('Page heading test', async ({ page }) => {
+  await page.goto('https://www.example.com')
+  const pageHeading = await page.locator('h1')
+  await expect(pageHeading).toHaveText('Example Domain')
+})
+
+test('Page heading visibility test', async ({ page }) => {
+  await page.goto('https://www.example.com')
+  const pageHeading = await page.locator('h1')
+  await expect(pageHeading).toBeVisible()
+})
+
 test('Page title test', async ({ page }) => {
   await page.goto('https://www.example.com')
-  const pageTitle = await page.locator('h1')
-  await expect(pageTitle).toContainText('Example Domain')
+  await expect(page).toHaveTitle('Example Domain')
 })
 
 test('No credentials entered error message test', async ({ page }) => {
@@ -12,7 +23,7 @@ test('No credentials entered error message test', async ({ page }) => {
   await page.click('text=Sign in')
 
   const errorMessage = await page.locator('.alert-error')
-  await expect(errorMessage).toContainText('Login and/or password are wrong.')
+  await expect(errorMessage).toHaveText('Login and/or password are wrong.')
 })
 
 test('Invalid credentials error message test', async ({ page }) => {
@@ -24,5 +35,10 @@ test('Invalid credentials error message test', async ({ page }) => {
   await page.click('text=Sign in')
 
   const errorMessage = await page.locator('.alert-error')
-  await expect(errorMessage).toContainText('Login and/or password are wrong.')
+  await expect(errorMessage).toHaveText('Login and/or password are wrong.')
+})
+
+test('URL no redirection test', async ({ page }) => {
+  await page.goto('https://www.example.com')
+  await expect(page).toHaveURL('https://www.example.com')
 })
