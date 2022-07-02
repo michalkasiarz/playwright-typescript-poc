094c747da6887a46f6c2fde967b276f697902a8e
michalkasiarz <42114632+michalkasiarz@users.noreply.github.com>
2022-07-02 13:30:48 +0200

    Tests/basic tests (#2)
    * prettier config modified
    
    * tests directory created along with one basic test
    
    * basic testfile renamed, test names refactored
    
    * new tests added, text assertion check fixed
    
    * organizing tests into suites
    
    * tests suites tagged as regression
    
    * playwright config file added

--- tests/basicTests.spec.ts
+++ tests/basicTests.spec.ts
@@ -1,28 +1,54 @@
 import { test, expect } from '@playwright/test'
 
-test('Page title test', async ({ page }) => {
-  await page.goto('https://www.example.com')
-  const pageTitle = await page.locator('h1')
-  await expect(pageTitle).toContainText('Example Domain')
+test.describe('Example.com basic test suite @regressionExample', () => {
+  test('Page heading test', async ({ page }) => {
+    await page.goto('https://www.example.com')
+    const pageHeading = await page.locator('h1')
+    await expect(pageHeading).toHaveText('Example Domain')
+  })
+
+  test('Page heading visibility test', async ({ page }) => {
+    await page.goto('https://www.example.com')
+    const pageHeading = await page.locator('h1')
+    await expect(pageHeading).toBeVisible()
+  })
+
+  test('Page title test', async ({ page }) => {
+    await page.goto('https://www.example.com')
+    await expect(page).toHaveTitle('Example Domain')
+  })
+
+  test('URL no redirection test', async ({ page }) => {
+    await page.goto('https://www.example.com')
+    await expect(page).toHaveURL('https://www.example.com')
+  })
+
+  test('Removed header non-visibility test', async ({ page }) => {
+    await page.goto('https://www.example.com')
+    const removedHeader = page.locator('text=Removed Domain Name')
+    await expect(removedHeader).not.toBeVisible()
+  })
 })
 
-test('No credentials entered error message test', async ({ page }) => {
-  await page.goto('http://zero.webappsecurity.com/')
-  await page.click('#signin_button')
-  await page.click('text=Sign in')
+test.describe('Zero.webappsecurity basic test suite @regressionZeroWebApp', () => {
+  test('No credentials entered error message test', async ({ page }) => {
+    await page.goto('http://zero.webappsecurity.com/')
+    await page.click('#signin_button')
+    await page.click('text=Sign in')
 
-  const errorMessage = await page.locator('.alert-error')
-  await expect(errorMessage).toContainText('Login and/or password are wrong.')
-})
+    const errorMessage = await page.locator('.alert-error')
+    await expect(errorMessage).toHaveText('Login and/or password are wrong.')
+  })
 
-test('Invalid credentials error message test', async ({ page }) => {
-  await page.goto('http://zero.webappsecurity.com/')
-  await page.click('#signin_button')
+  test('Invalid credentials error message test', async ({ page }) => {
+    await page.goto('http://zero.webappsecurity.com/')
+    await page.click('#signin_button')
 
-  await page.type('#user_login', 'testuser')
-  await page.type('#user_password', 'testpassword')
-  await page.click('text=Sign in')
+    await page.type('#user_login', 'testuser')
+    await page.type('#user_password', 'testpassword')
+    await page.click('text=Sign in')
 
-  const errorMessage = await page.locator('.alert-error')
-  await expect(errorMessage).toContainText('Login and/or password are wrong.')
+    const errorMessage = await page.locator('.alert-error')
+    await expect(errorMessage).toHaveText('Login and/or password are wrong.')
+  })
 })
