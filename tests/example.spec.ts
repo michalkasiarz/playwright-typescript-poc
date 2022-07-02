95384a6a873f6657d07e92a496194e4fe40778c0
Michal Kasiarz <michal.kasiarz@protonmail.com>
2022-07-02 11:20:42 +0200

    tests directory created along with one basic test

---
+++ tests/example.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test'
+
+test('First test', async ({ page }) => {
+  await page.goto('https://www.example.com')
+  const pageTitle = await page.locator('h1')
+  await expect(pageTitle).toContainText('Example Domain')
+})
