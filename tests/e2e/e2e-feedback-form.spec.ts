ad5e1585d3d641fbbcaf5e708c03b48a69f9113d
Michal Kasiarz <michal.kasiarz@protonmail.com>
2022-07-03 09:59:51 +0200

    submit feedback form test added

--- tests/e2e/e2e-feedback-form.spec.ts
+++ tests/e2e/e2e-feedback-form.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test.describe('Feedback form', () => {
+test.describe.parallel('Feedback form', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('http://zero.webappsecurity.com/')
     await page.click('#feedback')
@@ -23,5 +23,21 @@ test.describe('Feedback form', () => {
     await expect(emailInput).toBeEmpty()
     await expect(subjectInput).toBeEmpty()
     await expect(commentInput).toBeEmpty()
+  })
+
+  test('Submit feedback form test', async ({ page }) => {
+    const nameInput = await page.locator('#name')
+    const emailInput = await page.locator('#email')
+    const subjectInput = await page.locator('#subject')
+    const commentInput = await page.locator('#comment')
+
+    await nameInput.type('dummy name')
+    await emailInput.type('dummy email')
+    await subjectInput.type('dummy subject')
+    await commentInput.type('dummy comment')
+
+    await page.click('input[value="Send Message"]')
+
+    await page.waitForSelector('#feedback-title')
   })
 })
