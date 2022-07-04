d6bbfd98cf8697e0d8eb60fd315efe49aa5a32c4
michalkasiarz <42114632+michalkasiarz@users.noreply.github.com>
2022-07-04 05:15:10 +0200

    unused imports removed (#18)

--- tests/e2e/e2e-searchbox.spec.ts
+++ tests/e2e/e2e-searchbox.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test } from '@playwright/test'
 import { LandingPage } from '../../page-objects/LandingPage'
 import { validateNumberOfLinksFound } from '../../helpers'
 
