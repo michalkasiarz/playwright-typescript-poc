39b67f5b97f6255c50a6481f603793c193011d28
Michal Kasiarz <michal.kasiarz@protonmail.com>
2022-07-04 04:23:44 +0200

    page objects catalog structure rebuilt - components folder added

--- page-objects/components/PayBillsNavBarPage.ts
+++ page-objects/components/PayBillsNavBarPage.ts
@@ -2,25 +2,25 @@ import { expect, Locator, Page } from '@playwright/test'
 
 export class PayBillsNavBarPage {
   readonly page: Page
-  readonly paySavedPayeeTab: Locator
   readonly addNewPayeeTab: Locator
+  readonly paySavedPayeeTab: Locator
   readonly purchaseForeignCurrencyTab: Locator
 
   constructor(page: Page) {
     this.page = page
-    this.paySavedPayeeTab = page.locator('text="Pay Saved Payee"')
     this.addNewPayeeTab = page.locator('text="Add New Payee"')
+    this.paySavedPayeeTab = page.locator('text="Pay Saved Payee"')
     this.purchaseForeignCurrencyTab = page.locator(
       'text="Purchase Foreign Currency"'
     )
   }
 
-  async clickPaySavedPayeeTab() {
-    await this.paySavedPayeeTab.click()
-  }
-
   async clickAddNewPayeeTab() {
     await this.addNewPayeeTab.click()
+  }
+
+  async clickPaySavedPayeeTab() {
+    await this.paySavedPayeeTab.click()
   }
 
   async clickPurchaseForeignCurrencyTab() {
