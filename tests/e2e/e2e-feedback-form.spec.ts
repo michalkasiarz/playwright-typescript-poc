def213186d843ddf8a17ddd4f3021738d1c42a9a
Michal Kasiarz <michal.kasiarz@protonmail.com>
2022-07-04 05:14:40 +0200

    unused imports removed

--- tests/e2e/e2e-feedback-form.spec.ts
+++ tests/e2e/e2e-feedback-form.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test } from '@playwright/test'
 import { FeedbackPage } from '../../page-objects/FeedbackPage'
 import { LandingPage } from '../../page-objects/LandingPage'
 import { LoginPage } from '../../page-objects/LoginPage'
--- tests/e2e/e2e-filter-transactions.spec.ts
+++ tests/e2e/e2e-filter-transactions.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test } from '@playwright/test'
 import { AccountActivityShowTransactionsPage } from '../../page-objects/AccountActivityShowTransactionsPage'
 import { LandingPage } from '../../page-objects/LandingPage'
 import { LoginPage } from '../../page-objects/LoginPage'
